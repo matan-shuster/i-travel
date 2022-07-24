ae98895fd7ad5c257e70b5708d0e94077ce83f13
Segev Shalev <segevshalev@gmail.com>
2022-07-24 21:17:15 +0300

    json examples

---
+++ direaction.js
@@ -0,0 +1,12 @@
+const API_KEY = "AIzaSyAg8dTPhsF8dibkYVcWE-qsAYIBoTvRiec";
+/* https://maps.googleapis.com/maps/api/directions/json?
+                origin=32.07513236900241,34.77489829753883
+                &destination=32.07934145403047,34.768643386677404
+                &mode=transit
+                &language=en
+                &key=AIzaSyAg8dTPhsF8dibkYVcWE-qsAYIBoTvRiec
+*/
+
+/* &waypoints=Barossa+Valley,SA|Clare,SA|Connawarra,SA|McLaren+Vale,SA
+
+"waypoint_order": [ 3, 2, 0, 1 ] */
