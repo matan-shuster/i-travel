b743dff5eef0c389d97730ba4d546d6c5f7bd5b4
Itay Neuberger <itayneu@gmail.com>
2022-07-26 19:37:16 +0300

    feat: added search bar

--- client/src/components/AddEvent.jsx
+++ client/src/components/AddEvent.jsx
@@ -4,31 +4,53 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+
 function AddEvent() {
   const [selectedCategory, setSelectedCategory] = useState(0);
-  const categories = ["All", "Restaurants", "Cafes", "Hotels", "Museums"];
+  const categoriesList = ["All", "Restaurants", "Cafes", "Hotels", "Museums"];
 
-  const handleChange = (e) => setSelectedCategory(e.target.value);
+  const handleCategoryChange = (e) => setSelectedCategory(e.target.value);
 
   return (
-    <FormControl>
-      <InputLabel id="demo-simple-select-label">Category</InputLabel>
-      <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        value={selectedCategory}
-        label="Category"
-        onChange={handleChange}
-      >
-        {categories.map((item, index) => {
-          return (
-            <MenuItem key={index} value={index}>
-              {item}
-            </MenuItem>
-          );
-        })}
-      </Select>
-    </FormControl>
+    <Paper
+      component="form"
+      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+    >
+      <IconButton sx={{ p: "10px" }} aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search"
+        inputProps={{ "aria-label": "search google maps" }}
+      />
+      <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      <FormControl variant="filled" sx={{ m: 1, minWidth: 135 }} size="small">
+        <InputLabel id="demo-simple-select-filled-label">Category</InputLabel>
+        <Select
+          labelId="demo-simple-select-filled-label"
+          id="demo-simple-select-filled"
+          value={selectedCategory}
+          label="Category"
+          onChange={handleCategoryChange}
+        >
+          {categoriesList.map((item, index) => {
+            return (
+              <MenuItem key={index} value={index}>
+                {item}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+    </Paper>
   );
 }
 
