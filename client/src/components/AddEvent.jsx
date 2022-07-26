0695c4dade9c29d725ab55f31dd4428c784f74d8
Itay Neuberger <itayneu@gmail.com>
2022-07-26 20:27:19 +0300

    feat: search results cards

--- client/src/components/AddEvent.jsx
+++ client/src/components/AddEvent.jsx
@@ -10,47 +10,83 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
+import StarIcon from "@mui/icons-material/Star";
+import json from "./restaurants.json";
+
 function AddEvent() {
   const [selectedCategory, setSelectedCategory] = useState(0);
   const categoriesList = ["All", "Restaurants", "Cafes", "Hotels", "Museums"];
 
   const handleCategoryChange = (e) => setSelectedCategory(e.target.value);
 
+  const renderRatingStars = (rating) => {
+    let stars = [];
+
+    for (let i = 0; i < Math.round(rating); i++) {
+      stars.push(<StarIcon fontSize="small" />);
+    }
+
+    return stars;
+  };
+
   return (
-    <Paper
-      component="form"
-      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
-    >
-      <IconButton sx={{ p: "10px" }} aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search"
-        inputProps={{ "aria-label": "search google maps" }}
-      />
-      <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-      <FormControl variant="filled" sx={{ m: 1, minWidth: 135 }} size="small">
-        <InputLabel id="demo-simple-select-filled-label">Category</InputLabel>
-        <Select
-          labelId="demo-simple-select-filled-label"
-          id="demo-simple-select-filled"
-          value={selectedCategory}
-          label="Category"
-          onChange={handleCategoryChange}
-        >
-          {categoriesList.map((item, index) => {
-            return (
-              <MenuItem key={index} value={index}>
-                {item}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
-    </Paper>
+    <div>
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      >
+        <IconButton sx={{ p: "10px" }} aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search"
+          inputProps={{ "aria-label": "search google maps" }}
+        />
+        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <FormControl variant="filled" sx={{ m: 1, minWidth: 135 }} size="small">
+          <InputLabel id="demo-simple-select-filled-label">Category</InputLabel>
+          <Select
+            labelId="demo-simple-select-filled-label"
+            id="demo-simple-select-filled"
+            value={selectedCategory}
+            label="Category"
+            onChange={handleCategoryChange}
+          >
+            {categoriesList.map((category, index) => {
+              return (
+                <MenuItem key={`category-${index}`} value={index}>
+                  {category}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </Paper>
+      {json.results.map((result, index) => {
+        return (
+          <Card key={`result-${index}`} sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography color="text.secondary" gutterBottom>
+                {renderRatingStars(result.rating)}
+              </Typography>
+              <Typography variant="h6" component="div">
+                {result.name}
+              </Typography>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                {result.formatted_address}
+              </Typography>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </div>
   );
 }
 
