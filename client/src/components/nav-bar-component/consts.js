6561ef89325593f66bf2481d4382aa13143b06bb
Itay Neuberger <itayneu@gmail.com>
2022-07-29 12:59:33 +0300

    feat: event page route

--- client/src/components/nav-bar-component/consts.js
+++ client/src/components/nav-bar-component/consts.js
@@ -1,11 +1,13 @@
 export const ROUTES_MAPPING = {
   TRIP_LIST_CONTAINER: "/",
-  TRIP_EVENTS: "trip",
-  ADD_NEW_EVENT: "add_event",
+  TRIP_EVENTS: "/trip/:id",
+  EVENT: "/trip/:id/event/:id",
+  ADD_NEW_EVENT: "/trip/:id/add_new_event",
 };
 
 export const ROUTES = [
   { name: "TripList", route: ROUTES_MAPPING.TRIP_LIST_CONTAINER, key: 1 },
   { name: "TripEvents", route: ROUTES_MAPPING.TRIP_EVENTS, key: 2 },
-  { name: "AddNewEvent", route: ROUTES_MAPPING.ADD_NEW_EVENT, key: 3 },
+  { name: "Event", route: ROUTES_MAPPING.EVENT, key: 3 },
+  { name: "AddNewEvent", route: ROUTES_MAPPING.ADD_NEW_EVENT, key: 4 },
 ];
