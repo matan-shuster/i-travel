97fbd5e269b989ad2ae7248776e5809f39cad61f
Segev Shalev <segevshalev@gmail.com>
2022-07-29 13:37:55 +0300

    events components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import TripListContainer from "./components/trip-list-container/TripListContainer";
-import DUMMY_DATA from "./trips.json";
+import TripEventsContainer from "./components/trip-events-container/TripEventsContainer";
+import TRIPS_DUMMY_DATA from "./trips.json";
+import EVENTS_DUMMY_DATA from "./events,.json";
 function App() {
   return (
     <div className="App">
-      <TripListContainer data={DUMMY_DATA} />
+      <TripListContainer data={TRIPS_DUMMY_DATA} />
+      {/* <TripEventsContainer data={EVENTS_DUMMY_DATA} /> */}
     </div>
   );
 }
