853fa6b161ac091053f99610c1211be7f6941740
Itay Neuberger <itayneu@gmail.com>
2022-07-30 12:05:29 +0300

    feat: google places search api support

--- client/src/components/add-new-event/AddNewEvent.jsx
+++ client/src/components/add-new-event/AddNewEvent.jsx
@@ -1,23 +1,59 @@
 import React, { useState } from "react";
 import SearchBar from "../search-bar/SearchBar";
 import EventCard from "../event-card/EventCard";
+import apiService from "../../services/apiService";
 import DUMMY_PLACES_DATA from "../../places.json";
 
 function AddNewEvent() {
   const [expandedId, setExpandedId] = useState(-1);
   const [searchInput, setSearchInput] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("all");
+  const [places, setPlaces] = useState([]);
+
+  const handleSearchInputChange = async (e) => {
+    setSearchInput(e.target.value);
+    setExpandedId(-1);
+    if (e.target.value === "") {
+      setPlaces([]);
+    } else {
+      const placesList = await apiService.getPlaces(e.target.value);
+      setPlaces(placesList.results);
+    }
+  };
+
+  const handleSearchClear = () => {
+    setSearchInput("");
+    setExpandedId(-1);
+    setPlaces([]);
+  };
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+    setExpandedId(-1);
+  };
 
   return (
     <div style={{ textAlign: "left" }}>
       <SearchBar
         searchInput={searchInput}
-        setSearchInput={setSearchInput}
         selectedCategory={selectedCategory}
-        setSelectedCategory={setSelectedCategory}
-        setExpandedId={setExpandedId}
+        handleSearchInputChange={handleSearchInputChange}
+        handleSearchClear={handleSearchClear}
+        handleCategoryChange={handleCategoryChange}
       />
-      {DUMMY_PLACES_DATA.results
+      {Array.isArray(places) && places.length
+        ? places.map((place, index) => {
+            return (
+              <EventCard
+                place={place}
+                index={index}
+                expandedId={expandedId}
+                setExpandedId={setExpandedId}
+              />
+            );
+          })
+        : ""}
+      {/* {DUMMY_PLACES_DATA.results
         .filter(
           (place) =>
             (place.name.toLowerCase().includes(searchInput.toLowerCase()) ||
@@ -34,7 +70,7 @@ function AddNewEvent() {
               setExpandedId={setExpandedId}
             />
           );
-        })}
+        })} */}
     </div>
   );
 }
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,77 +1,83 @@
-
-const express = require('express');
-const TripdataManager = require('../services/tripdata_manager');
+const express = require("express");
+const TripdataManager = require("../services/tripdata_manager");
+const PlacesManager = require("../services/places_manager");
 const router = express.Router();
 
 //Event Router
 const tripdataManager = new TripdataManager();
 
-router.get('/events', async (req, res, next) => {
-    res.send(await tripdataManager.getEventList());
-    next()
+router.get("/events", async (req, res, next) => {
+  res.send(await tripdataManager.getEventList());
+  next();
 });
 
-router.post('/events', async (req, res, next) => {
-    const renderedEvents = await tripdataManager.createEvent(req.body.event)
-    res.status(200).json(renderedEvents)
+router.post("/events", async (req, res, next) => {
+  const renderedEvents = await tripdataManager.createEvent(req.body.event);
+  res.status(200).json(renderedEvents);
 });
-router.put('/events/:id', async (req, res) => {
-    await tripdataManager.updateEvent(req.body.id, req.body.event)
-    res.status(200).json(req.body.event)
+router.put("/events/:id", async (req, res) => {
+  await tripdataManager.updateEvent(req.body.id, req.body.event);
+  res.status(200).json(req.body.event);
 });
-router.delete('/events/:id', async (req, res) => {
-    await tripdataManager.deleteEvent(req.body.id)
-    res.status(200).json(req.body.id)
+router.delete("/events/:id", async (req, res) => {
+  await tripdataManager.deleteEvent(req.body.id);
+  res.status(200).json(req.body.id);
 });
-
 
 //Trip Router
-router.get('/trips', async (req, res, next) => {
-    res.send(await tripdataManager.getTripList())
-    next()
+router.get("/trips", async (req, res, next) => {
+  res.send(await tripdataManager.getTripList());
+  next();
 });
 
-router.post('/trips', async (req, res, next) => {
-    const renderedTrips = await tripdataManager.createTrip(req.body.trip)
-    res.status(200).json(renderedTrips)
+router.post("/trips", async (req, res, next) => {
+  const renderedTrips = await tripdataManager.createTrip(req.body.trip);
+  res.status(200).json(renderedTrips);
 });
-router.put('/trips/:id', async (req, res) => {
-    await tripdataManager.updateTrip(req.body.id, req.body.trip)
-    res.status(200).json(req.body.trip)
+router.put("/trips/:id", async (req, res) => {
+  await tripdataManager.updateTrip(req.body.id, req.body.trip);
+  res.status(200).json(req.body.trip);
 });
-router.delete('/trips/:id', async (req, res) => {
-    await tripdataManager.deleteTrip(req.body.id)
-    res.status(200).json(req.body.id)
+router.delete("/trips/:id", async (req, res) => {
+  await tripdataManager.deleteTrip(req.body.id);
+  res.status(200).json(req.body.id);
 });
 
 //User Router
-router.get('/users', async (req, res, next) => {
-    res.send(await tripdataManager.getUserList())
-    next()
+router.get("/users", async (req, res, next) => {
+  res.send(await tripdataManager.getUserList());
+  next();
 });
 
-router.get('/users/:id/trips', async (req, res, next) => {
-    res.send(await tripdataManager.getTripsWithEvents(req.params.id))
-    next()
+router.get("/users/:id/trips", async (req, res, next) => {
+  res.send(await tripdataManager.getTripsWithEvents(req.params.id));
+  next();
 });
-router.post('/users', async (req, res, next) => {
-    const renderedUsers = await tripdataManager.createUser(req.body.user)
-    res.status(200).json(renderedUsers)
+router.post("/users", async (req, res, next) => {
+  const renderedUsers = await tripdataManager.createUser(req.body.user);
+  res.status(200).json(renderedUsers);
 });
-router.put('/users/:id', async (req, res) => {
-    await tripdataManager.updateUser(req.body.id, req.body.user)
-    res.status(200).json(req.body.user)
+router.put("/users/:id", async (req, res) => {
+  await tripdataManager.updateUser(req.body.id, req.body.user);
+  res.status(200).json(req.body.user);
 });
-router.delete('/users/:id', async (req, res) => {
-    await tripdataManager.deleteUser(req.body.id)
-    res.status(200).json(req.body.id)
-});
-
-router.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Healthy'
-    });
+router.delete("/users/:id", async (req, res) => {
+  await tripdataManager.deleteUser(req.body.id);
+  res.status(200).json(req.body.id);
 });
 
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Healthy",
+  });
+});
+
+//Places Router
+const placesManager = new PlacesManager();
+
+router.get("/places/:query", async (req, res, next) => {
+  res.send(await placesManager.getPlaces(req.params.query));
+  next();
+});
 
 module.exports = router;
