adcb850129287892105145e6955c747dbcdf6b6c
matan-shuster <82678238+matan-shuster@users.noreply.github.com>
2022-08-01 20:21:41 +0300

    Changed add date to dialog and added transition

--- client/src/components/add-new-trip/AddNewTrip.jsx
+++ client/src/components/add-new-trip/AddNewTrip.jsx
@@ -3,36 +3,53 @@ import React, { useState, useCallback } from "react";
 import NewTripInputs from "./NewTripInputs";
 import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
+import {Dialog, Slide} from "@mui/material";
 
 export default function AddNewTrip({ onNewTripPressed, userID, newTripId }) {
   const [displayBtn, setDisplayBtn] = useState(true);
+  const [open, setOpen] = useState(false);
   const setDisplayBtnOn = useCallback(() => {
     setDisplayBtn(true);
   }, []);
 
   const addButtom = (
     <Fab
-      sx={{ bgcolor: "darkblue", color: "gray" }}
+      sx={{ bgcolor: "darkblue", color: "white" }}
       aria-label="add"
       onClick={() => {
         setDisplayBtn(false);
+        handleClickOpen();
       }}
     >
       <AddIcon />
     </Fab>
   );
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+  const handleClose = () => {
+    setOpen(false);
+    setDisplayBtn(true);
+  }
+  const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 
   return (
     <div>
       {displayBtn ? (
         addButtom
       ) : (
-        <NewTripInputs
-          onSubmitEvent={setDisplayBtnOn}
-          onNewTripPressed={onNewTripPressed}
-          userID={userID}
-          newTripId={++newTripId}
-        />
+          <Dialog open={open} onClose={handleClose} TransitionComponent={Transition}>
+            <NewTripInputs
+                onSubmitEvent={setDisplayBtnOn}
+                onNewTripPressed={onNewTripPressed}
+                userID={userID}
+                newTripId={++newTripId}
+            />
+          </Dialog>
+
+
       )}
     </div>
   );
